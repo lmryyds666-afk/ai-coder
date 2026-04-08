ee57fd59a69009a55d85c17c026652e0e1e23d5b
limengran3620 <2205470071@qq.com>
2026-04-08 17:30:10 +0800

    第八期 - 功能扩展 1。引入selenium + webDriverManager实现截图 2.编写通用截图服务 3.引入COS对象存储，编写CosManager对象存储

--- src/main/java/com/lmr/aicoder/service/impl/AppServiceImpl.java
+++ src/main/java/com/lmr/aicoder/service/impl/AppServiceImpl.java
@@ -19,6 +19,7 @@ import com.lmr.aicoder.model.enums.CodeGenTypeEnum;
 import com.lmr.aicoder.model.vo.AppVO;
 import com.lmr.aicoder.model.vo.UserVO;
 import com.lmr.aicoder.service.ChatHistoryService;
+import com.lmr.aicoder.service.ScreenshotService;
 import com.lmr.aicoder.service.UserService;
 import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.spring.service.impl.ServiceImpl;
@@ -61,6 +62,8 @@ public class AppServiceImpl extends ServiceImpl<AppMapper, App>  implements AppS
     private OrderedFormContentFilter formContentFilter;
     @Resource
     private StreamHandlerExecutor streamHandlerExecutor;
+    @Resource
+    private ScreenshotService screenshotService;
 
 
     public AppServiceImpl(UserService userService, AiCodeGeneratorFacade aiCodeGeneratorFacade) {
@@ -234,10 +237,35 @@ public class AppServiceImpl extends ServiceImpl<AppMapper, App>  implements AppS
         updateApp.setDeployedTime(LocalDateTime.now());
         boolean updateResult = this.updateById(updateApp);
         ThrowUtils.throwIf(!updateResult,ErrorCode.OPERATION_ERROR,"数据库应用部署失败");
+        // 10. 构建应用访问 URL
+        String appDeployUrl = String.format("%s/%s/", AppConstant.CODE_DEPLOY_HOST, deployKey);
+        // 11. 异步生成截图并更新应用封面
+        generateAppScreenshotAsync(appId, appDeployUrl);
+        return appDeployUrl;
 
-        //返回可访问的URL
-        return String.format("%s/%s",AppConstant.CODE_DEPLOY_HOST,deployKey);
     }
+
+    /**
+     * 异步生成应用截图并更新封面
+     *
+     * @param appId  应用ID
+     * @param appUrl 应用访问URL
+     */
+
+    public void generateAppScreenshotAsync(Long appId, String appUrl) {
+        // 使用虚拟线程异步执行
+        Thread.startVirtualThread(() -> {
+            // 调用截图服务生成截图并上传
+            String screenshotUrl = screenshotService.generateAndUploadScreenshot(appUrl);
+            // 更新应用封面字段
+            App updateApp = new App();
+            updateApp.setId(appId);
+            updateApp.setCover(screenshotUrl);
+            boolean updated = this.updateById(updateApp);
+            ThrowUtils.throwIf(!updated, ErrorCode.OPERATION_ERROR, "更新应用封面字段失败");
+        });
+    }
+
 
 
     /**
